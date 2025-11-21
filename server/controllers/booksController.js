a0d9a7ed04ff07bd642edac5989a5983f8f4f8ff
Blessing Omolara Abimbola <babimbola80@gmail.com>
2025-11-21 15:27:13 +0100

    Create booksController.js

---
+++ server/controllers/booksController.js
@@ -0,0 +1,22 @@
+import Book from "../models/Book.js";
+
+export const getBooks = async (req, res) => {
+    const books = await Book.find();
+    res.json(books);
+};
+
+export const addBook = async (req, res) => {
+    const newBook = new Book(req.body);
+    await newBook.save();
+    res.json(newBook);
+};
+
+export const updateBook = async (req, res) => {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedBook);
+};
+
+export const deleteBook = async (req, res) => {
+    await Book.findByIdAndDelete(req.params.id);
+    res.json({ message: "Book deleted" });
+};
