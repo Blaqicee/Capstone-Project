3bcad8ae1d44042f78aae6d394ddbfb55adef33e
Blessing Omolara Abimbola <babimbola80@gmail.com>
2025-11-21 15:26:46 +0100

    Create books.js

---
+++ server/routes/books.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getBooks, addBook, updateBook, deleteBook } from "../controllers/booksController.js";
+const router = express.Router();
+
+router.get("/", getBooks);
+router.post("/", addBook);
+router.put("/:id", updateBook);
+router.delete("/:id", deleteBook);
+
+export default router;
